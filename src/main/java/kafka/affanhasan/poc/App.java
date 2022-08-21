3cda93ee02fbd3f238700c6c43ce8129a05adbeb
季美来 <jimeilai@192.168.1.106>
2022-08-21 22:26:38 +0100

    use maven to create a kafka project

---
+++ src/main/java/kafka/affanhasan/poc/App.java
@@ -0,0 +1,13 @@
+package kafka.affanhasan.poc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
